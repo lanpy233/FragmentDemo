2ce3506bfffa04b0f5fc2254eb6210c37328389e
兰坡阳 <lanpoyang@meizu.com>
2017-10-18 11:26:49 +0800

    [#000000] Activity动态添加Fragment; 问题原因：　 解决方案： 验证建议：

--- app/src/main/java/test/yuan/com/fragmentdemo/MainActivity.java
+++ app/src/main/java/test/yuan/com/fragmentdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package test.yuan.com.fragmentdemo;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction =  getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.linearlayout1,new MainFragment1());
+        transaction.commit();
+
+        FragmentTransaction transaction2 =  getSupportFragmentManager().beginTransaction();
+        transaction2.add(R.id.linearlayout2,new MainFragment2());
+        transaction2.commit();
     }
 }
